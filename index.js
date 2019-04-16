ada0392194c1509344988cdc5496e2608d42fcd9
Azuel Galiotto <azuel@hotmail.it>
2019-04-16 11:35:58 +0200

    add html code

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const http = require('http');
 
 const server = http.createServer(function(req,res) {
     res.writeHead(200, {'Content-Type' : 'text/html'});
-    res.write('Ciao, world!');
+    res.write('<h1>Nome Cognome</h1>');
     res.end();
 });
 
