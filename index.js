28b48eb72465ea27d286f959cca87ade98827d01
Azuel Galiotto <azuel@hotmail.it>
2019-04-19 12:15:10 +0200

    add module fs

--- index.js
+++ index.js
@@ -1,19 +1,37 @@
 // Importo la libreria express con il require e la metto in una costante
 const express = require('express')
-let port = process.argv[3] || 3000
 const app = express()
+const fs = require('fs')
+let port = process.argv[2] || 8080
 
 
-//console.log(process.env.PORT)
-console.log(process.argv)
-if(process.argv[2] === 'v') {
-  console.log("MyApp version 0.0.1")
-}
-
-app.get('/', function (req, res) {
-  res.send('Hello World')
+app.get('/books', function (req, res) {
+  const filename = process.argv[3]
+  fs.readFile(filename, function(e, data) {
+      // 500 Internet Servers Error
+      if(e) return res.sendStatus(500)
+      try {
+          books = JSON.parse(data)
+      } catch (e) {
+          res.sendStatus(500);
+      }
+      res.json(books)
+  })
 })
+
+app.get('/menu', function (req, res) {
+    const filename = process.argv[3]
+    fs.readFile(filename, function(e, data) {
+        // 500 Internet Servers Error
+        if(e) return res.sendStatus(500)
+        try {
+            menu = JSON.parse(data)
+        } catch (e) {
+            res.sendStatus(500);
+        }
+        res.json(menu)
+    })
+  })
  
 app.listen(port)
-
 console.log(`Server running at http://127.0.0.1:${port}/`);
