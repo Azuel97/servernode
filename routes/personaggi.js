65fd6ed67a60d62b19a63ba6d13ea9e124ea5b5b
Azuel Galiotto <azuel@hotmail.it>
2019-05-03 12:06:14 +0200

    add params

--- routes/personaggi.js
+++ routes/personaggi.js
@@ -6,15 +6,24 @@ router.get('/', (req, res) => {
     let msg = 'Ciao'
 
     //console.log('RES', query)
+    if(query.key === '97979979797979797979') {
+        msg = `Mario rossi `
 
-    if(query.sesso === 'm') {
-        msg = `${msg} bel uomo`
-    }
-    if(query.colore){
-        msg = `${msg} ${query.colore} `
-    }
+        if(query.sesso === 'm') {
+            msg = `${msg} bel uomo`
+        }
+        if(query.colore){
+            msg = `${msg} ${query.colore} `
+        }
+    }   
 
     res.send(msg)
+})
+
+router.get('/:id', (req, res) => {
+    const id = req.params.id
+    console.log('ID : ', id)
+    res.send(`personaggio id : ${id}`)
 })
 
 
