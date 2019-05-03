997f8b3fcd8b33ad4efc97fd51d744b9b2bafb73
Azuel Galiotto <azuel@hotmail.it>
2019-05-03 13:58:07 +0200

    add personaggi external

--- routes/personaggi.js
+++ routes/personaggi.js
@@ -1,31 +1,21 @@
 const express = require('express')
 const router = express.Router();
+const characters = require('../data/personaggi')
+
 
 router.get('/', (req, res) => {
     const query = req.query
-    let msg = 'Ciao'
+    let personaggi = characters.characters
 
-    //console.log('RES', query)
-    if(query.key === '97979979797979797979') {
-        msg = `Mario rossi `
+    if('lastname' in query){
+        personaggi = personaggi.filter((personaggio) => {
+            return personaggio.lastname === query.lastname
+        })
+    }
 
-        if(query.sesso === 'm') {
-            msg = `${msg} bel uomo`
-        }
-        if(query.colore){
-            msg = `${msg} ${query.colore} `
-        }
-    }   
-
-    res.send(msg)
+    res.send(personaggi)
+    
 })
-
-router.get('/:id', (req, res) => {
-    const id = req.params.id
-    console.log('ID : ', id)
-    res.send(`personaggio id : ${id}`)
-})
-
 
 // Esporto come modulo
 module.exports = router
